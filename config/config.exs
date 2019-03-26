ec2355eef0408ac4933adcd3f74c587ceafc03ea
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-03-25 21:26:31 -0700

    Add filter pattern for sensitive data

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,6 @@ use Mix.Config
 
 config :exvcr,
   filter_request_headers: [],
-  filter_sensitive_data: []
+  filter_sensitive_data: [
+    [pattern: "apiKey=[A-Z0-9]+", placeholder: "apiKey=***"]
+  ]
